102ea04c3a09f2237daee54cd52bab93e679f8aa
Marco Antônio da Silva Júnor <marcoasjunior90@gmail.com>
2020-06-29 16:06:18 -0300

    vuetify e design inicial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,105 @@
 <template>
-  <div id="app">
+  <v-app>
+    <v-app-bar app color="primary" dark class="title">
+      Desafio DevOZ
+    </v-app-bar>
 
+    <v-content>
 
-  </div>
+      <v-card max-width="600" class="mx-auto mt-5">
+
+        <v-card-title>
+
+          <p class="subtitle">Digite o endereço abaixo:</p>
+
+        </v-card-title>
+
+        <v-card-text>
+
+          <v-form class="d-flex flex-column">
+
+            <v-text-field v-model="address" label="Endereço" outlined required></v-text-field>
+
+            <v-btn @click="getAreas" color="primary">Procurar</v-btn>
+
+          </v-form>
+
+        </v-card-text>
+
+      </v-card>
+
+      <v-card max-width="600" class="mx-auto mt-5">
+
+        <v-card-title>
+
+          <p class="body-1">Áreas em que o endereço informado está contido:</p>
+
+        </v-card-title>
+
+        <v-card-text>
+
+          <v-list>
+
+            <v-list-item-group color="primary">
+
+              <v-list-item v-for="(area, i) in areas" :key="i">
+
+                <v-list-item-icon>
+
+                  <v-icon>mdi-map</v-icon>
+
+                </v-list-item-icon>
+
+                <v-list-item-content>
+
+                  <v-list-item-title v-text="area"></v-list-item-title>
+
+                </v-list-item-content>
+
+              </v-list-item>
+
+            </v-list-item-group>
+
+          </v-list>
+
+        </v-card-text>
+
+      </v-card>
+
+    </v-content>
+
+  </v-app>
+
 </template>
 
 <script>
 
-
 export default {
+
   name: 'App',
-  
+
+  data: () => ({
+
+    address: null,
+    areas: []
+
+  }),
+
+  methods: {
+    
+    async getAreas() {
+
+      if (!this.address) return alert('Preencha o endereço.')
+
+      const demand = await this.$axios.get(`http://localhost:3333/demand/${this.address}`)
+
+      if (!demand.data) alert('Erro ao procurar áreas.')
+
+      this.areas = demand.data.areas
+    }
+
+  },
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
-  render: function (h) { return h(App) },
+  vuetify,
+  render: function (h) { return h(App) }
 }).$mount('#app')
